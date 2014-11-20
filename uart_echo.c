48b1c55cb84faeb42f7b910a80c0dafd8e592673
dillon <dillon.min@irdeto.com>
2014-11-20 12:08:24 +0800

    first version

---
+++ uart_echo.c
@@ -0,0 +1,105 @@
+//*****************************************************************************
+//
+// uart_echo.c - Example for reading data from and writing data to the UART in
+//               an interrupt driven fashion.
+//
+// Copyright (c) 2013-2014 Texas Instruments Incorporated.  All rights reserved.
+// Software License Agreement
+// 
+// Texas Instruments (TI) is supplying this software for use solely and
+// exclusively on TI's microcontroller products. The software is owned by
+// TI and/or its suppliers, and is protected under applicable copyright
+// laws. You may not combine this software with "viral" open-source
+// software in order to form a larger program.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
+// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+// DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 2.1.0.12573 of the EK-TM4C1294XL Firmware Package.
+//
+//*****************************************************************************
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stm32f0xx.h>
+
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>UART Echo (uart_echo)</h1>
+//!
+//! This example application utilizes the UART to echo text.  The first UART
+//! (connected to the USB debug virtual serial port on the evaluation board)
+//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
+//! the UART are transmitted back to the UART.
+//
+//*****************************************************************************
+
+//****************************************************************************
+//
+// System clock rate in Hz.
+//
+//****************************************************************************
+uint32_t g_ui32SysClock;
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+#define rt_hw_led1_on()   GPIO_SetBits(GPIOA, GPIO_Pin_4)
+#define rt_hw_led1_off()  GPIO_ResetBits(GPIOA, GPIO_Pin_4)
+void delay(int times)
+{
+	volatile long i,j;
+	int m;
+	for(m=0;m<times;m++)
+	for(i=0;i<100000;i++)
+		j=0;
+}
+//*****************************************************************************
+//
+// This example demonstrates how to send a string of data to the UART.
+//
+//*****************************************************************************
+int
+main(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+    /* Enable the GPIO_LED Clock */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA|RCC_AHBPeriph_GPIOB, ENABLE);
+
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_ResetBits(GPIOA, GPIO_Pin_9);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    //
+    // Loop forever echoing data through the UART.
+    //
+    while(1)
+    {
+		rt_hw_led1_on();
+		delay(10);
+		rt_hw_led1_off();
+		delay(10);
+    }
+}
